b1dc6924be24b1675327e60d2587f6e5f2544b3d
Caleb Dyck <dyck.caleb@gmail.com>
2013-10-13 15:05:00 -0700

    Changed vapour.sql to show that achievements and items are weak entities. Added primary keys to tables that were missing them.
    Schema (on Drive) and vapour.sql should match now.

--- vapour.sql
+++ vapour.sql
@@ -24,8 +24,10 @@ DROP TABLE IF EXISTS `achievement`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `achievement` (
   `title` varchar(255) NOT NULL,
+  `game_id` int(50) NOT NULL,
   `description` text,
-  PRIMARY KEY (`title`)
+  PRIMARY KEY (`title`, `game_id`),
+  CONSTRAINT `achievement_ibfk_1` FOREIGN KEY (`game_id`) REFERENCES `game` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -48,11 +50,12 @@ DROP TABLE IF EXISTS `achievement_unlock`;
 CREATE TABLE `achievement_unlock` (
   `date` datetime DEFAULT NULL,
   `achievement` varchar(255) DEFAULT NULL,
+  `game_id` int(50) DEFAULT NULL,
   `user` varchar(255) DEFAULT NULL,
-  KEY `achievement` (`achievement`),
-  KEY `user` (`user`),
+  PRIMARY KEY (`achievement`, `game_id`, `user`),
   CONSTRAINT `achievement_unlock_ibfk_1` FOREIGN KEY (`achievement`) REFERENCES `achievement` (`title`),
-  CONSTRAINT `achievement_unlock_ibfk_2` FOREIGN KEY (`user`) REFERENCES `user` (`username`)
+  CONSTRAINT `achievement_unlock_ibfk_2` FOREIGN KEY (`game_id`) REFERENCES `game` (`id`),
+  CONSTRAINT `achievement_unlock_ibfk_3` FOREIGN KEY (`user`) REFERENCES `user` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -66,6 +69,87 @@ LOCK TABLES `achievement_unlock` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `admin`
+--
+
+DROP TABLE IF EXISTS `admin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `admin` (
+  `username` varchar(255),
+  PRIMARY KEY (`username`),
+  CONSTRAINT `admin_ibfk_1` FOREIGN KEY (`username`) REFERENCES `user` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `admin`
+--
+
+LOCK TABLES `admin` WRITE;
+/*!40000 ALTER TABLE `admin` DISABLE KEYS */;
+/*!40000 ALTER TABLE `admin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `comment`
+--
+
+DROP TABLE IF EXISTS `comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comment` (
+  `id` int(50) NOT NULL AUTO_INCREMENT,
+  `discussion_id` int(50) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `body` text DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`, `discussion_id`),
+  CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`discussion_id`) REFERENCES `discussion` (`id`),
+  CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`username`) REFERENCES `user` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comment`
+--
+
+LOCK TABLES `comment` WRITE;
+/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `discussion`
+--
+
+DROP TABLE IF EXISTS `discussion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `discussion` (
+  `id` int(50) NOT NULL AUTO_INCREMENT,
+  `group_id` int(50) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `body` text DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`, `group_id`),
+  CONSTRAINT `discussion_ibfk_1` FOREIGN KEY (`group_id`) REFERENCES `group` (`id`),
+  CONSTRAINT `discussion_ibfk_2` FOREIGN KEY (`username`) REFERENCES `user` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `discussion`
+--
+
+LOCK TABLES `discussion` WRITE;
+/*!40000 ALTER TABLE `discussion` DISABLE KEYS */;
+/*!40000 ALTER TABLE `discussion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+--
 -- Table structure for table `friend`
 --
 
@@ -76,8 +160,7 @@ CREATE TABLE `friend` (
   `user_0` varchar(255) DEFAULT NULL,
   `user_1` varchar(255) DEFAULT NULL,
   `date` datetime DEFAULT NULL,
-  KEY `user_0` (`user_0`),
-  KEY `user_1` (`user_1`),
+  PRIMARY KEY (`user_0`, `user_1`),
   CONSTRAINT `friend_ibfk_1` FOREIGN KEY (`user_0`) REFERENCES `user` (`username`),
   CONSTRAINT `friend_ibfk_2` FOREIGN KEY (`user_1`) REFERENCES `user` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -115,161 +198,35 @@ CREATE TABLE `game` (
 -- Dumping data for table `game`
 --
 
-
---
--- Table structure for table `game_achievement`
---
-
-DROP TABLE IF EXISTS `game_achievement`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `game_achievement` (
-  `id` int(50) NOT NULL AUTO_INCREMENT,
-  `game_id` int(50) DEFAULT NULL,
-  `achievement` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `game_id` (`game_id`),
-  KEY `achievement` (`achievement`),
-  CONSTRAINT `game_achievement_ibfk_1` FOREIGN KEY (`game_id`) REFERENCES `game` (`id`),
-  CONSTRAINT `game_achievement_ibfk_2` FOREIGN KEY (`achievement`) REFERENCES `achievement` (`title`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `game_achievement`
---
-
-LOCK TABLES `game_achievement` WRITE;
-/*!40000 ALTER TABLE `game_achievement` DISABLE KEYS */;
-/*!40000 ALTER TABLE `game_achievement` ENABLE KEYS */;
+LOCK TABLES `game` WRITE;
+/*!40000 ALTER TABLE `game` DISABLE KEYS */;
+/*!40000 ALTER TABLE `game` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `game_item`
+-- Table structure for table `game_purchase`
 --
 
-DROP TABLE IF EXISTS `game_item`;
+DROP TABLE IF EXISTS `game_purchase`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `game_item` (
-  `id` int(50) NOT NULL AUTO_INCREMENT,
-  `game_id` int(50) DEFAULT NULL,
-  `item` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `game_id` (`game_id`),
-  KEY `item` (`item`),
-  CONSTRAINT `game_item_ibfk_1` FOREIGN KEY (`game_id`) REFERENCES `game` (`id`),
-  CONSTRAINT `game_item_ibfk_2` FOREIGN KEY (`item`) REFERENCES `item` (`title`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `game_item`
---
-
-LOCK TABLES `game_item` WRITE;
-/*!40000 ALTER TABLE `game_item` DISABLE KEYS */;
-/*!40000 ALTER TABLE `game_item` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `item`
---
-
-DROP TABLE IF EXISTS `item`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `item` (
-  `title` varchar(255) NOT NULL,
-  `description` text,
-  PRIMARY KEY (`title`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `item`
---
-
-LOCK TABLES `item` WRITE;
-/*!40000 ALTER TABLE `item` DISABLE KEYS */;
-/*!40000 ALTER TABLE `item` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `item_unlock`
---
-
-DROP TABLE IF EXISTS `item_unlock`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `item_unlock` (
-  `date` datetime DEFAULT NULL,
-  `item` varchar(255) DEFAULT NULL,
+CREATE TABLE `game_purchase` (
   `user` varchar(255) DEFAULT NULL,
-  KEY `item` (`item`),
-  KEY `user` (`user`),
-  CONSTRAINT `item_unlock_ibfk_1` FOREIGN KEY (`item`) REFERENCES `item` (`title`),
-  CONSTRAINT `item_unlock_ibfk_2` FOREIGN KEY (`user`) REFERENCES `user` (`username`)
+  `game` int(50) DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  PRIMARY KEY (`user`, `game`),
+  CONSTRAINT `game_purchase_ibfk_1` FOREIGN KEY (`game`) REFERENCES `game` (`id`),
+  CONSTRAINT `game_purchase_ibfk_2` FOREIGN KEY (`user`) REFERENCES `user` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `item_unlock`
+-- Dumping data for table `game_purchase`
 --
 
-LOCK TABLES `item_unlock` WRITE;
-/*!40000 ALTER TABLE `item_unlock` DISABLE KEYS */;
-/*!40000 ALTER TABLE `item_unlock` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `user`
---
-
-DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user` (
-  `username` varchar(255) NOT NULL,
-  `is_banned` tinyint(1) DEFAULT NULL,
-  `cc_num` varchar(255) DEFAULT NULL,
-  `join_date` datetime DEFAULT NULL,
-  `account_balance` int(10) DEFAULT NULL,
-  `billing_address` varchar(511) DEFAULT NULL,
-  PRIMARY KEY (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `user`
---
-
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `admin`
---
-
-DROP TABLE IF EXISTS `admin`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `admin` (
-  `username` varchar(255),
-  PRIMARY KEY (`username`),
-  CONSTRAINT `admin_ibfk_1` FOREIGN KEY (`username`) REFERENCES `user` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `admin`
---
-
-LOCK TABLES `admin` WRITE;
-/*!40000 ALTER TABLE `admin` DISABLE KEYS */;
-/*!40000 ALTER TABLE `admin` ENABLE KEYS */;
+LOCK TABLES `game_purchase` WRITE;
+/*!40000 ALTER TABLE `game_purchase` DISABLE KEYS */;
+/*!40000 ALTER TABLE `game_purchase` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -323,87 +280,85 @@ LOCK TABLES `group_members` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `discussion`
+-- Table structure for table `item`
 --
 
-DROP TABLE IF EXISTS `discussion`;
+DROP TABLE IF EXISTS `item`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `discussion` (
-  `id` int(50) NOT NULL AUTO_INCREMENT,
-  `gid` int(50) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `title` varchar(255) DEFAULT NULL,
-  `body` text DEFAULT NULL,
-  `date` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`, `gid`),
-  CONSTRAINT `discussion_ibfk_1` FOREIGN KEY (`gid`) REFERENCES `group` (`id`),
-  CONSTRAINT `discussion_ibfk_2` FOREIGN KEY (`username`) REFERENCES `user` (`username`)
+CREATE TABLE `item` (
+  `title` varchar(255) NOT NULL,
+  `game_id` int(50) DEFAULT NULL,
+  `description` text,
+  PRIMARY KEY (`title`, `game_id`),
+  CONSTRAINT `item_ibfk_1` FOREIGN KEY (`game_id`) REFERENCES `game` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `discussion`
+-- Dumping data for table `item`
 --
 
-LOCK TABLES `discussion` WRITE;
-/*!40000 ALTER TABLE `discussion` DISABLE KEYS */;
-/*!40000 ALTER TABLE `discussion` ENABLE KEYS */;
+LOCK TABLES `item` WRITE;
+/*!40000 ALTER TABLE `item` DISABLE KEYS */;
+/*!40000 ALTER TABLE `item` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `comment`
+-- Table structure for table `item_unlock`
 --
 
-DROP TABLE IF EXISTS `comment`;
+DROP TABLE IF EXISTS `item_unlock`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `comment` (
-  `id` int(50) NOT NULL AUTO_INCREMENT,
-  `did` int(50) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `body` text DEFAULT NULL,
+CREATE TABLE `item_unlock` (
   `date` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`, `did`),
-  CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`did`) REFERENCES `discussion` (`id`),
-  CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`username`) REFERENCES `user` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `comment`
---
-
-LOCK TABLES `comment` WRITE;
-/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
-/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `game_purchase`
---
-
-DROP TABLE IF EXISTS `game_purchase`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `game_purchase` (
+  `item` varchar(255) DEFAULT NULL,
+  `game_id` int(50) DEFAULT NULL,
   `user` varchar(255) DEFAULT NULL,
-  `game` int(50) DEFAULT NULL,
-  `date` datetime DEFAULT NULL,
-  PRIMARY KEY (`user`, `game`),
-  CONSTRAINT `game_purchase_ibfk_1` FOREIGN KEY (`game`) REFERENCES `game` (`id`),
-  CONSTRAINT `game_purchase_ibfk_2` FOREIGN KEY (`user`) REFERENCES `user` (`username`)
+  PRIMARY KEY (`item`, `game_id`, `user`),
+  CONSTRAINT `item_unlock_ibfk_1` FOREIGN KEY (`item`) REFERENCES `item` (`title`),
+  CONSTRAINT `item_unlock_ibfk_2` FOREIGN KEY (`game_id`) REFERENCES `game` (`id`),
+  CONSTRAINT `item_unlock_ibfk_3` FOREIGN KEY (`user`) REFERENCES `user` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `game_purchase`
+-- Dumping data for table `item_unlock`
 --
 
-LOCK TABLES `game_purchase` WRITE;
-/*!40000 ALTER TABLE `game_purchase` DISABLE KEYS */;
-/*!40000 ALTER TABLE `game_purchase` ENABLE KEYS */;
+LOCK TABLES `item_unlock` WRITE;
+/*!40000 ALTER TABLE `item_unlock` DISABLE KEYS */;
+/*!40000 ALTER TABLE `item_unlock` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `username` varchar(255) NOT NULL,
+  `is_banned` tinyint(1) DEFAULT NULL,
+  `cc_num` varchar(255) DEFAULT NULL,
+  `join_date` datetime DEFAULT NULL,
+  `account_balance` int(10) DEFAULT NULL,
+  `billing_address` varchar(511) DEFAULT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
 
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
