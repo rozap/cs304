ceef0290a0b8b8236e993c6cffe9c0d101922a05
Kyle Malong <kylepdm@gmail.com>
2013-11-22 12:43:06 -0800

    PRAISE BE TO THE LORD OF GABEN. LORD OF GAMES. LORD OF HOSTS. PRAISE GABEN PRAAAAIIISE

--- make_gaben.py
+++ make_gaben.py
@@ -7,6 +7,38 @@ def add_gaben(db):
 	}
 	db.users.insert_user(vals)
 
+def get_games(db):
+    games = db.games.get_games(limit=100000)
+    return games
+
+def giv_gaben_games(db):
+    vals = {
+    'username' : 'LORDGABEN',
+    }
+    for game in get_games(db):
+        vals['game'] = game['id']
+        db.game_purchase.insert_game_purchase(vals)
+
+def giv_gaben_achievements(db):
+    vals = {
+    'user' : 'LORDGABEN',
+    }
+    for game in get_games(db):
+        vals['game_id'] = game['id']
+        for achievement in db.achievements.get_game_achievements(game['id']):
+            vals['achievement'] = achievement['title']
+            db.achievements.unlock_achievement(vals)
+
+def giv_gaben_items(db):
+    vals = {
+    'user' : 'LORDGABEN',
+    }
+    for game in get_games(db):
+        vals['game_id'] = game['id']
+        for item in db.items.get_game_items(game['id']):
+            vals['item'] = item['title']
+            db.items.insert_item_unlock(vals)
+
 if __name__ == "__main__":
     db = Database()
     add_gaben(db)
