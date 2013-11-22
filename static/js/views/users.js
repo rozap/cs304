e374dc2d3ad4b540e40abddca813f42b900ff717
Kyle Malong <kylepdm@gmail.com>
2013-11-21 16:00:50 -0800

    Added D3, got some visualizations

--- static/js/views/users.js
+++ static/js/views/users.js
@@ -5,24 +5,29 @@ define([
 	'collections',
 	'models',
 	'views',
+	'libs/d3',
 
 	'text!templates/users/login.html',
 	'text!templates/users/register.html',
 	'text!templates/users/profile.html',
 	'text!templates/users/community.html'
 
-], function($, _, Backbone, Collections, Models, Views, LoginViewTemplate, RegisterViewTemplate, ProfileViewTemplate, CommunityViewTemplate) {
+], function($, _, Backbone, Collections, Models, Views, D3, LoginViewTemplate, RegisterViewTemplate, ProfileViewTemplate, CommunityViewTemplate) {
 
 	// highscores
 	var CommunityView = Views.AbstractView.extend({
 		template: _.template(CommunityViewTemplate),
 
 		el: '#main',
+		data: [],
 
 		events: {
 			"click .show_games": "showGamesOnly",
 			"click .show_items": "showItemsOnly",
 			"click .show_achievements": "showAchievementsOnly",
+			"click .show_min": "showMinOnly",
+			"click .show_max": "showMaxOnly",
+			"click .show_avg": "showAvgOnly"
 		},
 
 		initialize: function(app) {
@@ -38,8 +43,106 @@ define([
 			ctx.showGamesOnly = ctx.showGamesOnly;
 			ctx.showItemsOnly = ctx.showItemsOnly;
 			ctx.showAchievementsOnly = ctx.showAchievementsOnly;
+			ctx.showMinOnly = ctx.showMinOnly;
+			ctx.showMaxOnly = ctx.showMaxOnly;
+			ctx.showAvgOnly = ctx.showAvgOnly;
 			this.$el.html(this.template(ctx));
+			this.data = [ 
+				this.model.get('game_purchase_min').community_min,
+				this.model.get('item_unlock_min').community_min,
+				this.model.get('achievement_unlock_min').community_min,
+
+				this.model.get('game_purchase_max').community_max,
+				this.model.get('item_unlock_max').community_max,
+				this.model.get('achievement_unlock_max').community_max,
+
+				this.model.get('game_purchase_avg').community_avg,
+				this.model.get('item_unlock_avg').community_avg,
+				this.model.get('achievement_unlock_avg').community_avg,
+				];
+
+ 
+			console.log(this.data);
 			//Views.AbstractView.prototype.render.call(this, ctx);
+		},
+
+		showMinOnly: function(event) {
+			this.render({
+				showMinOnly: true,
+				showMaxOnly: false,
+				showAvgOnly: false
+			});
+			
+			var svgContainer = d3.select(".d3").append("svg")
+				.attr("width", 500)
+				.attr("height", 500);
+
+			var circle = svgContainer.append("circle").style("fill", "steelred")
+				.attr("cx", 30)
+				.attr("cy", 30)
+				.attr("r", this.data[0]);
+
+			var circle = svgContainer.append("circle").style("fill", "steelblue")
+				.attr("cx", 30 + 30)
+				.attr("cy", 30 + 30)
+				.attr("r", this.data[1]);
+
+			var circle = svgContainer.append("circle").style("fill", "steelgreen")
+				.attr("cx", 30 + 30)
+				.attr("cy", 30 + 30)
+				.attr("r", this.data[2]);
+		},
+
+		showMaxOnly: function(event) {
+			this.render({
+				showMinOnly: false,
+				showMaxOnly: true,
+				showAvgOnly: false
+			});
+			var svgContainer = d3.select(".d3").append("svg")
+				.attr("width", 500)
+				.attr("height", 500);
+
+			var circle = svgContainer.append("circle").style("fill", "steelred")
+				.attr("cx", 30)
+				.attr("cy", 30)
+				.attr("r", this.data[3]);
+
+			var circle = svgContainer.append("circle").style("fill", "steelblue")
+				.attr("cx", 30 + 30)
+				.attr("cy", 30 + 30)
+				.attr("r", this.data[4]);
+
+			var circle = svgContainer.append("circle").style("fill", "steelgreen")
+				.attr("cx", 30 + 30)
+				.attr("cy", 30 + 30)
+				.attr("r", this.data[5]);
+		},
+
+		showAvgOnly: function(event) {
+			this.render({
+				showMinOnly: false,
+				showMaxOnly: false,
+				showAvgOnly: true
+			});
+			var svgContainer = d3.select(".d3").append("svg")
+				.attr("width", 200)
+				.attr("height", 200);
+
+			var circle = svgContainer.append("circle").style("fill", "steelred")
+				.attr("cx", 30)
+				.attr("cy", 30)
+				.attr("r", this.data[6]);
+
+			var circle = svgContainer.append("circle").style("fill", "steelblue")
+				.attr("cx", 30 + 30)
+				.attr("cy", 30 + 30)
+				.attr("r", this.data[7]);
+
+			var circle = svgContainer.append("circle").style("fill", "steelgreen")
+				.attr("cx", 30 + 30)
+				.attr("cy", 30 + 30)
+				.attr("r", this.data[8]);
 		},
 
 		showGamesOnly: function(event) {
